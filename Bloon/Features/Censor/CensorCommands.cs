9ea0c1fd0d586d172980f9286eac98a163d64708
Christopher Satchell <satchell028@gmail.com>
2021-04-23 12:33:29 +0200

    Hotfix: Fix "censor list" exceeding embed limits

--- Bloon/Features/Censor/CensorCommands.cs
+++ Bloon/Features/Censor/CensorCommands.cs
@@ -1,6 +1,7 @@
 namespace Bloon.Features.Censor
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Bloon.Core.Commands.Attributes;
@@ -15,6 +16,8 @@ namespace Bloon.Features.Censor
     [ModuleLifespan(ModuleLifespan.Transient)]
     public class CensorCommands : BaseCommandModule
     {
+        private const int CensorsPerEmbed = 15;
+
         private readonly BloonContext db;
         private readonly Censorer censorer;
 
@@ -67,14 +70,30 @@ namespace Bloon.Features.Censor
 
         [Command("list")]
         [Description("Retrieves the list of censors")]
-        public async Task ListAsync(CommandContext ctx)
+        public async Task ListAsync(CommandContext ctx, int page = 1)
         {
+            page = page < 1 ? 1 : page;
+
+            int total = await this.db.Censors.CountAsync();
+            List<Censor> found = await this.db.Censors
+                .Skip((page - 1) * CensorsPerEmbed)
+                .Take(CensorsPerEmbed)
+                .ToListAsync();
+
+            string description = found.Count == 0
+                ? "PEBCAK!"
+                : $"```\n{found.ToMarkdownTable()}```";
+
             DiscordEmbedBuilder embed = new DiscordEmbedBuilder
             {
                 Color = new DiscordColor(217, 187, 19),
                 Timestamp = DateTime.UtcNow,
-                Title = "Current Censor Patterns",
-                Description = $"```md\n{this.db.Censors.ToList().ToMarkdownTable()}```",
+                Title = $"Current Censor Patterns (Page {page}/{(int)Math.Ceiling((decimal)total / CensorsPerEmbed)})",
+                Description = description,
+                Footer = new DiscordEmbedBuilder.EmbedFooter
+                {
+                    Text = "View pages with \".censor list <page>\"",
+                },
             };
 
             await ctx.RespondAsync(embed);
