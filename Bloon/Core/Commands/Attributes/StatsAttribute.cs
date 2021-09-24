b2b3ca0ec5df0ea39039f3b0b0d71a985bec1622
Cody Fraker <codyfraker95@gmail.com>
2021-09-24 17:36:22 -0400

    Enable stats command to be ran inside of a thread

--- Bloon/Core/Commands/Attributes/StatsAttribute.cs
+++ Bloon/Core/Commands/Attributes/StatsAttribute.cs
@@ -2,6 +2,7 @@ namespace Bloon.Core.Commands.Attributes
 {
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Bloon.Variables;
     using Bloon.Variables.Channels;
     using DSharpPlus.CommandsNext;
     using DSharpPlus.CommandsNext.Attributes;
@@ -15,26 +16,34 @@ namespace Bloon.Core.Commands.Attributes
     {
         public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
         {
-            List<ulong> availChannels = new List<ulong>
-            {
-                SBGChannels.General,
-                SBGChannels.TradingPost,
-                SBGChannels.CompetitiveMatches,
-                SBGChannels.AUG,
-                SBGChannels.Wiki,
-                SBGChannels.Offtopic,
-                BloonChannels.CommandCentre,
-                BloonChannels.Commands,
-                BloonChannels.Ground0,
-            };
-            if (availChannels.Contains(ctx.Channel.Id) || ctx.Channel.IsPrivate)
+            bool asdf = ctx.Channel.IsThread;
+
+            if (ctx.Guild.Id == Guilds.Bloon || ctx.Channel.IsPrivate)
             {
                 return Task.FromResult(true);
             }
-            else
+
+            if (ctx.Guild.Id == Guilds.SBG)
             {
-                return Task.FromResult(false);
+                List<ulong> availChannels = new List<ulong>
+                {
+                    SBGChannels.General,
+                    SBGChannels.TradingPost,
+                    SBGChannels.CompetitiveMatches,
+                    SBGChannels.AUG,
+                    SBGChannels.Wiki,
+                    SBGChannels.Offtopic,
+                };
+                if (availChannels.Contains(ctx.Channel.Id))
+                {
+                    return Task.FromResult(true);
+                }
+                else
+                {
+                    return Task.FromResult(false);
+                }
             }
+            return Task.FromResult(false);
         }
     }
 }
