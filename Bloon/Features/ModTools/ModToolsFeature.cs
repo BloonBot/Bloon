e3a6f41d8a0b73b9f34019b74f745d0df26c09b0
Cody Fraker <codyfraker95@gmail.com>
2021-09-24 17:28:14 -0400

    server mods can now use user info context menus

--- Bloon/Features/ModTools/ModToolsFeature.cs
+++ Bloon/Features/ModTools/ModToolsFeature.cs
@@ -3,16 +3,20 @@ namespace Bloon.Features.ModTools
     using System.Threading.Tasks;
     using Bloon.Core.Discord;
     using Bloon.Core.Services;
+    using Bloon.Variables;
     using DSharpPlus;
     using DSharpPlus.CommandsNext;
+    using DSharpPlus.SlashCommands;
 
     public class ModToolsFeature : Feature
     {
         private readonly CommandsNextExtension cNext;
+        private readonly SlashCommandsExtension slash;
 
         public ModToolsFeature(DiscordClient dClient)
         {
             this.cNext = dClient.GetCommandsNext();
+            this.slash = dClient.GetSlashCommands();
         }
 
         public override string Name => "Mod Tools";
@@ -35,6 +39,8 @@ namespace Bloon.Features.ModTools
             //this.cNext.RegisterCommands<MuteUser>();
             //this.cNext.RegisterCommands<KickUser>();
             //this.cNext.RegisterCommands<BanUser>();
+
+            this.slash.RegisterCommands<UserInfoMenu>(Guilds.SBG);
             return base.Enable();
         }
     }
